2b1760d20855c262328a607e90cbae8503272660
Alex Myers <alexcmyers@arizona.edu>
2024-02-18 16:14:29 -0700

    Added some comments to better explain what the plot is made with

--- Capstone.R
+++ Capstone.R
@@ -5,8 +5,21 @@
 
 library(ggplot2)
 
-#Create data
+#Create data and put it in data frame
 Little_women <- data.frame(
   Name=c("Med","Jo","Beth","Amy"),
-  Times.Mentioned=c(683,1355,459,645)
+  Times.Mentioned=c(683,1355,459,645) 
 )
+
+
+# Create table. Bar graph, X Name, Y TimesMentioned, colors for each name, 
+ggplot(data = Little_women, mapping = aes(x=Name, y=Times.Mentioned,fill=Name)) +
+  geom_bar(stat='identity') +
+  labs(
+    x="Names", #x axis title
+    y="Times Mentioned", #y axis title 
+    title="Figure 1", # Main title of figure
+    color="In the Book" # Title of Legend
+  )
+
+  
