09ad7852dcfb2465d6e3b75cdb670b597df43355
Alex Myers <alexcmyers@arizona.edu>
2024-02-18 16:10:48 -0700

    Bar Graph added for the amount of times name was in little women

---
+++ Capstone.R
@@ -0,0 +1,12 @@
+# Capstone
+# Alex Myers
+# alexcmyers@arizona.edu  
+# 2024-02-18
+
+library(ggplot2)
+
+#Create data
+Little_women <- data.frame(
+  Name=c("Med","Jo","Beth","Amy"),
+  Times.Mentioned=c(683,1355,459,645)
+)
